b6e9b21a31154ddf54d08b74444604c4e1558f75
Kaan Chinar <128641309+kaanchinar@users.noreply.github.com>
2025-01-07 20:37:57 +0400

    add: initial music player

---
+++ ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+
+namespace MusicAppWPF.ViewModels
+{
+    public class WelcomeViewModel
+    {
+        public ICommand NavigateToAlbumsCommand { get; }
+        public ICommand NavigateToArtistsCommand { get; }
+        public ICommand NavigateToAllTracksCommand { get; }
+
+        public WelcomeViewModel()
+        {
+            NavigateToAlbumsCommand = new RelayCommand(OnNavigateToAlbums);
+            NavigateToArtistsCommand = new RelayCommand(OnNavigateToArtists);
+            NavigateToAllTracksCommand = new RelayCommand(OnNavigateToAllTracks);
+        }
+
+        public event Action<string> NavigateRequested;
+
+        private void OnNavigateToAlbums()
+        {
+            NavigateRequested?.Invoke("Views/Albums.xaml");
+        }
+
+        private void OnNavigateToArtists()
+        {
+            NavigateRequested?.Invoke("Views/Artists.xaml");
+        }
+
+        private void OnNavigateToAllTracks()
+        {
+            NavigateRequested?.Invoke("Views/AllTracks.xaml");
+        }
+    }
+}
--- Views/Welcome.xaml.cs
+++ Views/Welcome.xaml.cs
@@ -1,11 +1,26 @@
-﻿using System.Windows.Controls;
+﻿using MusicAppWPF.ViewModels;
+using System.Windows.Controls;
 
 namespace MusicAppWPF.Views;
 
 public partial class Welcome : Page
 {
+    private readonly WelcomeViewModel _viewModel;
     public Welcome()
     {
         InitializeComponent();
+        // Initialize ViewModel
+        _viewModel = new WelcomeViewModel();
+
+        // Set DataContext
+        DataContext = _viewModel;
+
+        // Subscribe to Navigation Event
+        _viewModel.NavigateRequested += OnNavigateRequested;
+    }
+    private void OnNavigateRequested(string pageUri)
+    {
+        // Navigate the Frame
+        ContentFrame.Navigate(new Uri(pageUri, UriKind.Relative));
     }
 }
